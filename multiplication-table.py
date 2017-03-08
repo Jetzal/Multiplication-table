8268a1919d5b613d6957bfca9ada3662da1f880e
Jetzal <Jetzal@users.noreply.github.com>
2017-03-08 13:41:35 -0500

    Updated from Brython Server: 3/8/2017 1:41:35 PM

--- multiplication-table.py
+++ multiplication-table.py
@@ -1,7 +1,7 @@
 """
 multiplication-table.py
 Author: jett
-Credits: noone
+Credit: noone
 Assignment: muktiplcaiton
 
 Write and submit a Python program that prints a multiplication table. The user 
